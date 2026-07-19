42e2cc8610a0ea4dcd4327e51aff2ec1406e68fb
Саша O. <si.ovcharuk2@gmail.com>
2026-07-19 12:06:28 +0300

    Add project files.

---
+++ DelegatePractise2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DelegatePractise2
+{
+    public delegate T OperationsDelegate<T>(T a, T b);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(5, 3, Add);
+            Calculate(5, 3, Minus);
+            Calculate(5, 3, Multiply);
+        }
+
+        static T Calculate<T>(T a, T b, OperationsDelegate<T> operation)
+        {
+            return operation(a, b);
+        }
+
+        static T Add<T>(T a, T b)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            return x + y;
+        }
+
+        static T Minus<T>(T a, T b)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            return x - y;
+        }
+
+        static T Multiply<T>(T a, T b)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            return x * y;
+        }
+    }
+}
